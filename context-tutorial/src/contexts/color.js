a05bbf5f721acb061beb0f942c42b35f95698a47
kimpompu <domathing@naver.com>
2025-08-14 22:21:32 +0900

    250814 Context

---
+++ context-tutorial/src/contexts/color.js
@@ -0,0 +1,31 @@
+import { Children, createContext, useState } from "react";
+
+const ColorContext = createContext({
+  state: { color: "black", subcolor: "red" },
+  action: {
+    setColor: () => {},
+    setSubcolor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState("black");
+  const [subcolor, setSubcolor] = useState("red");
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor },
+  };
+
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider, ColorConsumer export
+export { ColorProvider, ColorConsumer };
+
+export default ColorContext;
